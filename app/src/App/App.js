aa2a893dac06e1bb4d90bc85394b459eac45e819
Taha Afzal <tahaafzal5@hotmail.com>
2023-11-01 20:59:29 -0500

    Add Body

--- app/src/App/App.js
+++ app/src/App/App.js
@@ -1,11 +1,12 @@
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import Body from '../Components/Body';
 
 function App() {
   return (
     <div className="app">
       <Header />
-
+      <Body />
       <Footer />
     </div>
   );
