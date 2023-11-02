1b5809b773a0948393960b5f34cb6bb92fbcf342
Taha Afzal <tahaafzal5@hotmail.com>
2023-11-02 16:50:48 -0500

    Add NetWorthOutput
    Closes #6

---
+++ app/src/Components/NetWorthOutput.js
@@ -0,0 +1,21 @@
+import circleQuestionMark from '../Assets/Images/circle-question-mark.png';
+
+function NetWorthOutput({ title, assetsTotal, liabilitiesTotal }) {
+    const assets = parseFloat(assetsTotal) || 0;
+    const liabilities = parseFloat(liabilitiesTotal) || 0;
+
+    const netWorth = (assets - liabilities).toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
+
+    return (
+        <div>
+            <h2>{title} <img src={circleQuestionMark} width={15} /> </h2>
+            {netWorth}
+
+        </div>
+    );
+}
+
+export default NetWorthOutput;
