262475e175988f72f4f824e8f6901bb7372653bf
Taha Afzal <tahaafzal5@hotmail.com>
2023-11-02 16:51:07 -0500

    Add MainSection
    Closes #7

--- app/src/Components/MainSection.js
+++ app/src/Components/MainSection.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
 import '../Styles/MainSection.css';
+import NetWorthGroup from './NetWorthGroup.js'
+import NetWorthOutput from './NetWorthOutput.js'
 
 function MainSection() {
+    const assets = ['Checking Accounts', 'Savings Accounts', 'Money Market Accounts', "CDs", "Bonds", "Other"];
+    const liabilities = ["Credit Card Balances", "Outstanding Bills", "Home Mortgage", "Car Loans", "Student Loans", "Other"];
+    const [assetsTotal, setAssetsTotal] = useState(0);
+    const [liabilitiesTotal, setLiabilitiesTotal] = useState(0);
+
+    const handleTotalChange = (category, total) => {
+        if (category === 'Assets') {
+            setAssetsTotal(total);
+        } else if (category === 'Liabilities') {
+            setLiabilitiesTotal(total);
+        }
+    };
     return (
-        <div className="main">
+        <div className="main-section">
+            <NetWorthGroup category="Assets" items={assets} onTotalChange={(total) => handleTotalChange('Assets', total)} />
+            <NetWorthGroup category="Liabilities" items={liabilities} onTotalChange={(total) => handleTotalChange('Liabilities', total)} />
+            <NetWorthOutput title="Net Worth" assetsTotal={assetsTotal} liabilitiesTotal={liabilitiesTotal} />
         </div>
     );
 }
