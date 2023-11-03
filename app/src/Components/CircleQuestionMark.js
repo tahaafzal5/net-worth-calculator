6b6757e786d4f60b4df625c3d3c717845ed1499b
Taha Afzal <tahaafzal5@hotmail.com>
2023-11-03 12:11:43 -0500

    Make CircleQuestionMark its own Component

---
+++ app/src/Components/CircleQuestionMark.js
@@ -0,0 +1,9 @@
+import circleQuestionMark from '../Assets/Images/circle-question-mark.png';
+
+function CircleQuestionMark() {
+    return (
+        <img src={circleQuestionMark} width={15} alt='question mark' />
+    );
+}
+
+export default CircleQuestionMark;
--- app/src/Components/NetWorthGroup.js
+++ app/src/Components/NetWorthGroup.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import NetWorthItem from './NetWorthItem'
-import circleQuestionMark from '../Assets/Images/circle-question-mark.png';
+import CircleQuestionMark from './CircleQuestionMark';
 
 function NetWorthGroup({ category, items, onTotalChange }) {
     const [total, setTotal] = useState(0);
@@ -12,7 +12,7 @@ function NetWorthGroup({ category, items, onTotalChange }) {
 
     return (
         <div>
-            <h2>{category} <img src={circleQuestionMark} width={15} alt='question mark' /> </h2>
+            <h2>{category} <CircleQuestionMark /> </h2>
             <NetWorthItem category={category} items={items} onTotalChange={handleTotalChange} />
         </div>
     );
